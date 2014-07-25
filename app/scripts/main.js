818820894f41e5f9b7cfe50b4a15a2db54f26b1e
touchRED <connor@duncans-mbp.gateway.2wire.net>
2014-07-25 02:30:09 -0400

    basic first person controller

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,15 +6,24 @@ $(document).ready(function(){
 
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+	$('body').append(renderer.domElement);
+	$('body').mousemove(function(e){
+		// console.log(e.pageX + ' , ' + e.pageY);
+		// console.log(camera.rotation.y + ' , ' + camera.rotation.x);
+		camera.rotation.y = e.pageX * -0.005 - (window.innerWidth/2 * (window.innerWidth * 0.0000065));
+		camera.rotation.x = e.pageY * 0.005 + (window.innerHeight/2 * (window.innerHeight * 0.0000074));
+	});
 
-	var geometry = new THREE.CubeGeometry(3,3,3);
+	var geometry = new THREE.SphereGeometry(25,15,15);
 	var material = new THREE.MeshBasicMaterial({ color:0x3EBB3E, wireframe: true});
-	var cube = new THREE.Mesh(geometry, material);
+	var sphere = new THREE.Mesh(geometry, material);
 
-	scene.add(cube);
+	scene.add(sphere);
 
-	camera.position.z = 4;
+	camera.position.z = 50;
+
+	// console.log(camera.rotation.x + "," + camera.rotation.y + "," + camera.rotation.z);
+	// console.log(camera.position.x + "," + camera.position.y + "," + camera.position.z);
 
 	var keysDown = {};
 
@@ -30,16 +39,20 @@ $(document).ready(function(){
 		requestAnimationFrame(render);
 
 		if(37 in keysDown){
-			cube.rotation.y -= 0.025;
+			camera.position.x -= Math.cos(camera.rotation.y);
+			camera.position.z -= Math.sin(camera.rotation.y); 
 		}
 		if(38 in keysDown){
-			cube.rotation.x -= 0.025;
+			camera.position.x += Math.sin(camera.rotation.y);
+			camera.position.z += Math.cos(camera.rotation.y); 
 		}
 		if(39 in keysDown){
-			cube.rotation.y += 0.025;
+			camera.position.x += Math.cos(camera.rotation.y);
+			camera.position.z += Math.sin(camera.rotation.y); 
 		}
 		if(40 in keysDown){
-			cube.rotation.x += 0.025;
+			camera.position.x -= Math.sin(camera.rotation.y);
+			camera.position.z -= Math.cos(camera.rotation.y); 
 		}
 		renderer.render(scene, camera);
 	};
